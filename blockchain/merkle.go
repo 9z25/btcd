a8f3c9a84ebf1811baac2e94cffacecb557a4919
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:25:14 -0800

    Update merkle.go

--- blockchain/merkle.go
+++ blockchain/merkle.go
@@ -9,9 +9,9 @@ import (
 	"fmt"
 	"math"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/txscript"
+	"github.com/9z25/btcutil"
 )
 
 const (
