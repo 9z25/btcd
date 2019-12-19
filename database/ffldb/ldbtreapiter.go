285830726172eff8fc54d9728c4f9213b0b7fdb1
9z25 <rsenecaljr@gmail.com>
2019-12-19 12:53:09 -0800

    Update ldbtreapiter.go

--- database/ffldb/ldbtreapiter.go
+++ database/ffldb/ldbtreapiter.go
@@ -5,9 +5,9 @@
 package ffldb
 
 import (
-	"github.com/btcsuite/btcd/database/internal/treap"
-	"github.com/btcsuite/goleveldb/leveldb/iterator"
-	"github.com/btcsuite/goleveldb/leveldb/util"
+	"github.com/9z25/btcd/database/internal/treap"
+	"github.com/9z25/goleveldb/leveldb/iterator"
+	"github.com/9z25/goleveldb/leveldb/util"
 )
 
 // ldbTreapIter wraps a treap iterator to provide the additional functionality
