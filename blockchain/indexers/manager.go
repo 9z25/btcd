c232f22b3300fc7e2221a75e08caf2723ba52a9e
9z25 <rsenecaljr@gmail.com>
2019-12-19 12:58:03 -0800

    Update manager.go

--- blockchain/indexers/manager.go
+++ blockchain/indexers/manager.go
@@ -8,11 +8,11 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/database"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/blockchain"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/database"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 var (
