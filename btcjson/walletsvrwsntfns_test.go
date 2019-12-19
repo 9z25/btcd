e5139c0f97eca89da046f1a54b926b9139c4ac31
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:54:06 -0800

    Update walletsvrwsntfns_test.go

--- btcjson/walletsvrwsntfns_test.go
+++ btcjson/walletsvrwsntfns_test.go
@@ -11,7 +11,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcjson"
+	"github.com/9z25/btcd/btcjson"
 )
 
 // TestWalletSvrWsNtfns tests all of the chain server websocket-specific
