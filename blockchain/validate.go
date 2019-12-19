42b09be6d121fbf94bf9b53909c6a5bc042d83a1
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:34:37 -0800

    Update validate.go

--- blockchain/validate.go
+++ blockchain/validate.go
@@ -11,11 +11,11 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/txscript"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 const (
