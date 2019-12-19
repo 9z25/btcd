393bcf8f6693ca6d7c3a66d08e3cd385a4545784
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:44:52 -0800

    Update btcwalletextcmds_test.go

--- btcjson/btcwalletextcmds_test.go
+++ btcjson/btcwalletextcmds_test.go
@@ -11,7 +11,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcjson"
+	"github.com/9z25/btcd/btcjson"
 )
 
 // TestBtcWalletExtCmds tests all of the btcwallet extended commands marshal and
