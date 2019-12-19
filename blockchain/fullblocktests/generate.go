ea5a88535dda30b8c1aaa85441e04c4f10808001
9z25 <rsenecaljr@gmail.com>
2019-12-19 13:04:37 -0800

    Update generate.go

--- blockchain/fullblocktests/generate.go
+++ blockchain/fullblocktests/generate.go
@@ -18,13 +18,13 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/btcec"
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/blockchain"
+	"github.com/9z25/btcd/btcec"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/txscript"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 const (
