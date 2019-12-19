3361f5941a8784a745d4a585810ec81f664cd10a
9z25 <rsenecaljr@gmail.com>
2019-12-19 12:54:53 -0800

    Update doc.go

--- chaincfg/doc.go
+++ chaincfg/doc.go
@@ -25,8 +25,8 @@
 //          "fmt"
 //          "log"
 //
-//          "github.com/btcsuite/btcutil"
-//          "github.com/btcsuite/btcd/chaincfg"
+//          "github.com/9z25/btcutil"
+//          "github.com/9z25/btcd/chaincfg"
 //  )
 //
 //  var testnet = flag.Bool("testnet", false, "operate on the testnet Bitcoin network")
