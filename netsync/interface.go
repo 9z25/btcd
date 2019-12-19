ee182629e5b7355fda8d5feeced485e20388819c
9z25 <rsenecaljr@gmail.com>
2019-12-19 11:42:18 -0800

    Update interface.go

--- netsync/interface.go
+++ netsync/interface.go
@@ -5,13 +5,13 @@
 package netsync
 
 import (
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/mempool"
-	"github.com/btcsuite/btcd/peer"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/blockchain"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/mempool"
+	"github.com/9z25/btcd/peer"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 // PeerNotifier exposes methods to notify peers of status changes to
