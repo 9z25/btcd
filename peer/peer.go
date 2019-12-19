7955026dd7d8b036a346e940691c7567262ea85d
9z25 <rsenecaljr@gmail.com>
2019-12-19 13:07:32 -0800

    Update peer.go

--- peer/peer.go
+++ peer/peer.go
@@ -18,10 +18,10 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
+	"github.com/9z25/btcd/blockchain"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/wire"
 	"github.com/btcsuite/go-socks/socks"
 	"github.com/davecgh/go-spew/spew"
 )
