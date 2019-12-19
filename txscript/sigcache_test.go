1fd594fb200398d96916d537c80dcbec48008931
9z25 <rsenecaljr@gmail.com>
2019-12-18 20:39:15 -0800

    Update sigcache_test.go

--- txscript/sigcache_test.go
+++ txscript/sigcache_test.go
@@ -8,8 +8,8 @@ import (
 	"crypto/rand"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcec"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/btcec"
+	"github.com/9z25/btcd/chaincfg/chainhash"
 )
 
 // genRandomSig returns a random message, a signature of the message under the
