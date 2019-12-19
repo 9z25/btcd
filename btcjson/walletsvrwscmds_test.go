f6b832b1ba91913703e0c3f545048a378d3a3639
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:52:58 -0800

    Update walletsvrwscmds_test.go

--- btcjson/walletsvrwscmds_test.go
+++ btcjson/walletsvrwscmds_test.go
@@ -11,7 +11,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcjson"
+	"github.com/9z25/btcd/btcjson"
 )
 
 // TestWalletSvrWsCmds tests all of the wallet server websocket-specific
