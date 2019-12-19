25a2af1d4704f1ab55bec73d0b3a045a57ddf279
9z25 <rsenecaljr@gmail.com>
2019-12-18 20:53:09 -0800

    Update msggetcfilters.go

--- wire/msggetcfilters.go
+++ wire/msggetcfilters.go
@@ -7,7 +7,7 @@ package wire
 import (
 	"io"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/chaincfg/chainhash"
 )
 
 // MaxGetCFiltersReqRange the maximum number of filters that may be requested in
