61577d9cf1eb05f138be6633d7f9f6eaf2357879
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:17:07 -0800

    Update chainio_test.go

--- blockchain/chainio_test.go
+++ blockchain/chainio_test.go
@@ -11,8 +11,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/btcsuite/btcd/database"
-	"github.com/btcsuite/btcd/wire"
+	"github.com/9z25/btcd/database"
+	"github.com/9z25/btcd/wire"
 )
 
 // TestErrNotInMainChain ensures the functions related to errNotInMainChain work
