0a44fb59fdcd6661efc553bcabd1323bfd4f8a7c
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:16:43 -0800

    Update chainio.go

--- blockchain/chainio.go
+++ blockchain/chainio.go
@@ -12,10 +12,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/database"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/database"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 const (
