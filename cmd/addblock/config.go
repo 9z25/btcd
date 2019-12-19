efa6d4a3d09f5d1915671c84202ab7df9b74e70e
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:57:15 -0800

    Update config.go

--- cmd/addblock/config.go
+++ cmd/addblock/config.go
@@ -9,11 +9,11 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/database"
-	_ "github.com/btcsuite/btcd/database/ffldb"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/database"
+	_ "github.com/9z25/btcd/database/ffldb"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 	flags "github.com/jessevdk/go-flags"
 )
 
