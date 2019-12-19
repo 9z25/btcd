c2204732110b1e57daf588127cb44694232ae66f
9z25 <rsenecaljr@gmail.com>
2019-12-18 20:17:08 -0800

    Update estimatefee_test.go

--- mempool/estimatefee_test.go
+++ mempool/estimatefee_test.go
@@ -9,10 +9,10 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/mining"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/mining"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 // newTestFeeEstimator creates a feeEstimator with some different parameters
