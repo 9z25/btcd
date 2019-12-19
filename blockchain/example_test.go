de355fcf7de9dfbdcf2ae20b68f872a2ad6e8910
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:21:53 -0800

    Update example_test.go

--- blockchain/example_test.go
+++ blockchain/example_test.go
@@ -10,11 +10,11 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/database"
-	_ "github.com/btcsuite/btcd/database/ffldb"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/blockchain"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/database"
+	_ "github.com/9z25/btcd/database/ffldb"
+	"github.com/9z25/btcutil"
 )
 
 // This example demonstrates how to create a new chain instance and use
