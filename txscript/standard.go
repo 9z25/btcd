2c4bbe0bc617366553f27a7e24bb340d6b31979a
9z25 <rsenecaljr@gmail.com>
2019-12-18 20:40:53 -0800

    Update standard.go

--- txscript/standard.go
+++ txscript/standard.go
@@ -7,9 +7,9 @@ package txscript
 import (
 	"fmt"
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 const (
