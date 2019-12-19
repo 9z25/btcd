e0b6286cafc649035d57759340c6eff4ea44b667
9z25 <rsenecaljr@gmail.com>
2019-12-18 20:46:08 -0800

    Update msgblock.go

--- wire/msgblock.go
+++ wire/msgblock.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/chaincfg/chainhash"
 )
 
 // defaultTransactionAlloc is the default size used for the backing array
