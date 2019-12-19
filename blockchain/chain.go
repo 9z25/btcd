7c8be54e0d8b1d3998d332487a491cc4c402255b
9z25 <rsenecaljr@gmail.com>
2019-12-18 19:15:28 -0800

    Update chain.go

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -11,12 +11,12 @@ import (
 	"sync"
 	"time"
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/database"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/database"
+	"github.com/9z25/btcd/txscript"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 const (
