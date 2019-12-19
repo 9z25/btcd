2a3f5e75f8bbc2b5f32ae4cb356ce7bc7081e874
9z25 <rsenecaljr@gmail.com>
2019-12-18 20:20:57 -0800

    Update policy_test.go

--- mining/policy_test.go
+++ mining/policy_test.go
@@ -8,10 +8,10 @@ import (
 	"encoding/hex"
 	"testing"
 
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/blockchain"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 // newHashFromStr converts the passed big-endian hex string into a
